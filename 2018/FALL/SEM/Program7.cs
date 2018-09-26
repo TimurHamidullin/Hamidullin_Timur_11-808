a3fd28eee7d3382241454962718e04d5a3c7774c
Timur Hamidullin <timurxamidullin115@gmail.com>
2018-09-26 22:55:13 +0300

    Program7-8

---
+++ 2018/FALL/SEM/Program7.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program7
+    {
+        static void Main(string[] args)
+        {
+            double k = Double.Parse(Console.ReadLine());
+            double b = Double.Parse(Console.ReadLine());
+
+            // параллельный и перпендикулярный векторы из начала координат с Х = 1
+            Console.WriteLine("1 " + k);
+            Console.WriteLine("1 " + (-1/k));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2018/FALL/SEM/Program8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program8
+    {
+       // public string userInput;
+        
+        static void Main(string[] args)
+        {
+            double x = FindXOfIntersection(Console.ReadLine());
+
+            Console.WriteLine($"X = {x}");
+            Console.WriteLine($"Y = {0.5 * x + 1}");
+            Console.ReadKey();
+        }
+
+        static double FindXOfIntersection(string userInput)
+        {
+            string[] input = userInput.Split();
+            double k = double.Parse(input[0]);
+            double b = double.Parse(input[1]);
+            double xA = double.Parse(input[2]);
+            double yA = double.Parse(input[3]);
+
+            // готовая выведенная формула нахождения Х
+            return (xA / k + yA - b) / (k + 1 / k);
+        }
+    }
+}
