74c1280c865dc69ce44d98080bd9042ae0e74d18
TimurHamidullin <42980379+TimurHamidullin@users.noreply.github.com>
2018-10-07 23:10:12 +0300

    Add files via upload

---
+++ 2018/FALL/SEM/Expr10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int findSumOf3 = (3 + 999) * (1000 - 1) / 6;
+            int findSumOf5 = (5 + 995) * (1000 - 1) / 10;
+            int findSumOf15 = (15 + 985) * (1000 - 1) / 30;
+            int sum = findSumOf3 + findSumOf5 - findSumOf15;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2018/FALL/SEM/Expr11.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // allow to change
+            double hours = 12;
+            double minutes = 45;
+
+            if (hours >= 12) hours -= 12;
+
+            double minArrowPos = minutes / 60;
+            double hourArrowPos = (hours  + minArrowPos) / 12;
+
+            double degrees = Math.Abs(hourArrowPos - minArrowPos) * 360;
+
+            if (degrees < 180)
+                Console.Write(degrees);
+            else
+                Console.Write(360 - degrees);
+
+            Console.ReadKey();
+
+        }
+    }
+}
